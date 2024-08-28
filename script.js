87e19438ce27a8c8765a7550474ff361d8129eaf
Leonardo Alves <91397470+doshino@users.noreply.github.com>
2024-08-28 13:01:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function funcao()
 {
     alert("Funcionou");
 }
+
+function random(){
+    
+    const quadrado = document.getElementById("quadrado");
+    const cores = ["red", "green", "yellow", "purple", "orange"];
+    const corAtual = quadrado.style.backgroundColor;
+    let novaCor = corAtual;
+
+    while (novaCor === corAtual) {
+            novaCor = cores[Math.floor(Math.random() * cores.length)];
+    }
+
+    quadrado.style.backgroundColor = novaCor;
+}
